6668841f8554d6d5f0d662d1581d7010d80335eb
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-05-04 10:18:04 -0700

    Added main files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var MarketItem = (function () {
+    function MarketItem() {
+    }
+    return MarketItem;
+}());
+var a = (function () {
+    function a() {
+    }
+    return a;
+}());
---
+++ main.ts
@@ -0,0 +1,9 @@
+class MarketItem{
+    Name: string;
+    Description: string;
+    Price: number;
+    Seller: string;
+}
+class a{
+    Name:string;
+}
