83cfe7367505472d58af8eacd046ccd719214e30
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-05-04 11:55:53 -0700

    Finished validation function

--- js/main.js
+++ js/main.js
@@ -12,10 +12,31 @@ function validateData() {
     var desc = document.getElementById("desc");
     var price = document.getElementById("price");
     var seller = document.getElementById("seller");
+    if (name.value[0] != " " && desc.value[0] != " " && seller.value[0] != " " && isNaN(Number(price.value)) == false) {
+        if (name && name.value && desc && desc.value && seller && seller.value) {
+            return true;
+        }
+    }
+    if (isNaN(Number(price.value)) != false) {
+        displayError("Price input can not be converted to number", 2000);
+    }
+    else {
+        displayError("String inputs can not contain whitespace before the first character or be blank", 2000);
+    }
     return false;
 }
 function displayItem(item) {
 }
+function displayError(desc, time) {
+    var error = document.getElementById("Error");
+    while (error.textContent != "") { }
+    error.textContent = desc;
+    setTimeout(function () {
+        error.textContent = "";
+    }, time);
+}
 function createMarketItem() {
-    return MarketItem;
+    if (validateData()) {
+        var newItem = new MarketItem();
+    }
 }
--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ class MarketItem{
 }
 
 window.onload = function(){
-    let addBtn = <HTMLElement>document.querySelector("input[type=button]")
+    let addBtn = <HTMLElement>document.querySelector("input[type=button]");
     addBtn.onclick = createMarketItem;
 }
 
@@ -15,6 +15,16 @@ function validateData():boolean{
     let desc = <HTMLInputElement>document.getElementById("desc");
     let price = <HTMLInputElement>document.getElementById("price");
     let seller = <HTMLInputElement>document.getElementById("seller");
+    if (name.value[0] != " " && desc.value[0] != " "  && seller.value[0] != " " && isNaN(Number(price.value)) == false){
+        if (name && name.value && desc && desc.value && seller && seller.value){
+            return true;
+        }
+    }
+    if (isNaN(Number(price.value)) != false){
+        displayError("Price input can not be converted to number", 2000);
+    } else {
+        displayError("String inputs can not contain whitespace before the first character or be blank", 2000);
+    }
     return false;
 }
 
@@ -22,7 +32,17 @@ function displayItem(item:MarketItem):void{
 
 }
 
-function createMarketItem():MarketItem{
+function displayError(desc: string, time:number):void{
+    let error = <HTMLElement>document.getElementById("Error");
+    while (error.textContent != ""){}
+    error.textContent = desc;
+    setTimeout(() => {
+        error.textContent = "";
+    }, time);
+}
 
-    return MarketItem;
+function createMarketItem():void{
+    if (validateData()){
+        let newItem = new MarketItem();
+    }
 }
